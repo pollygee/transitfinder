04c9c1f4b73c03806b6324552f4b59bb0b956fcb
Polly Sileo <pollysileo@gmail.com>
2015-06-19 15:02:22 -0400

    added cors to gemfile, and headers application

--- server.rb
+++ server.rb
@@ -1,6 +1,6 @@
 require 'sinatra/base'
 require 'tilt/erubis' # Fixes a warning
-#require 'rack/cors'
+require 'rack/cors'
 require 'pry'
 require './db/setup'
 require './lib/all'
@@ -8,7 +8,16 @@ require 'haversine'
 require 'httparty'
 
 class TransportApp < Sinatra::Base
+  before do
+    headers["Content-Type"] = "application/json"
+  end
 
+  use Rack::Cors do
+    allow do
+      origins '*'
+      resource '*', headers: :any, methods: :get
+    end
+  end
   def how_far a_long, a_lat, b_long, b_lat
     dis = Haversine.distance(a_long, a_lat, b_long, b_lat)
     dis.to_miles
@@ -17,8 +26,6 @@ class TransportApp < Sinatra::Base
   def train_station_info station_code
     HTTParty.get("https://api.wmata.com/StationPrediction.svc/json/GetPrediction/#{station_code}", query: { api_key: "d311c928b8364eff80d7462f7938b2b1" })
   end
-
-    
   
   get "/train" do
     user_long = params["long"].to_f
@@ -34,23 +41,13 @@ class TransportApp < Sinatra::Base
 
     sorted_list = trains_and_distance.sort_by {|hsh| hsh[:distance]}
     close_train_list= sorted_list.first(3)
-    
-    # winner_list.each do |train|
-    #   top_three << train
-    # end
+
     full_station_info = sorted_list.first(3)
     close_train_list.each do |upcoming_trains|
-      station_info   = HTTParty.get("https://api.wmata.com/StationPrediction.svc/json/GetPrediction/#{upcoming_trains["code"]}", query: { api_key: "d311c928b8364eff80d7462f7938b2b1" })
-      # destination    = station_info["DestinationName"]
-      # line_color     = station_info["Line"]
-      # time_to_depart = station_info["Min"]
-      #final_train_information = {destination: destination, line_color: line_color, time_to_depart: time_to_depart}
+      station_info   = HTTParty.get("https://api.wmata.com/StationPrediction.svc/json/GetPrediction/#{upcoming_trains["code"]}", query: { api_key: "d311c928b8364eff80d7462f7938b2b1" })      
       upcoming_trains[:next_train] = station_info
     end
     final = close_train_list.to_json
-  end
-
-  get "/closest_bike" do
   end
 end
 
